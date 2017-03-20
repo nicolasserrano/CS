5b77e414dff4eebdf06bcf2857fa15d9b12c9426
nicolasserrano <nicolas.serrano.barcena@gmail.com>
2017-03-20 10:12:51 +0100

    Create README.js

---
+++ tools/SPLG/README.js
@@ -0,0 +1,20 @@
+# Simple Procedural Language for Graphics (SPLG)
+
+## Example: 
+P1 = point(3,5)
+P2 = point(6,8)
+line(P1,P2)
+Line1 = line(P1,P2)
+draw(Line1)
+polyline(P1,P2,...)
+polygon(P1,P2,...)
+pattern(style,P1,P2,...)
+arc(P1,radio,angle)
+Assembly1 = assemble(P1, line1, polygon1,...)
+translate(Assembly1, x, y)
+rotate(Assembly1, angle)
+scale(Assembly1,scaleX, scaleY)
+text(P1, "text")
+image(P1, image)
+
+[Example](https://nicolasserrano.github.io/CS/tools/SPLG/splg0.html)
